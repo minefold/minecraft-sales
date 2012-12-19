3cdad74b5a0ee4988e3e51c8bd1bf838409868e6
Chris Lloyd <christopher.lloyd@gmail.com>
2012-12-18 17:57:38 -0800

    Fix table.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ end
 get '/sales.csv' do
   header = "day,sales\n"
 
-  data = DB[:users].order(:at).reverse.limit(365 * 3).to_a
+  data = DB[:sales].order(:at).reverse.limit(365 * 3).to_a
       .group_by {|row| row[:at].strftime("%d-%m-%y") }
       .map {|day, rows| [day, rows.map {|r| r[:total]}.max] }
       .sort_by {|a,b| a[0] <=> b[0] }
